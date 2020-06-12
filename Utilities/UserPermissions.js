5961888a37bc9d0bdae7614d809e663c4171dd88
Sam Davey <samkdavey@gmail.com>
2020-06-12 15:38:24 +0100

    Modified user permissions

--- Utilities/UserPermissions.js
+++ Utilities/UserPermissions.js
@@ -1,9 +1,9 @@
-import Constants from 'expo-constants';
-import * as Permissions from 'expo-permissions';
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
 
 class UserPermissions {
     getCameraPermission = async () => {
-        if(Constants.platform.ios){
+        if(Constants.platform.ios || Constants.platform.android ){
             const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
 
             if(status != "granted"){
