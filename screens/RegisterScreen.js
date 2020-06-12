da2a3d8e9be9992083d162f9e1fc094cf0863647
Sam Davey <samkdavey@gmail.com>
2020-06-12 15:37:30 +0100

    Added switch and dropdown menu

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, TextInput, TouchableOpacity, Image, StatusBar } from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Image, StatusBar, Switch, ScrollView } from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
-import * as firebase from 'firebase';
-import UserPermissions from '../Utilities/UserPermissions';
+import * as firebase from "firebase";
+import UserPermissions from '../utilities/UserPermissions';
 import * as ImagePicker from 'expo-image-picker';
+//import { Dropdown } from 'react-native-material-dropdown';
+
 
 export default class RegisterScreen extends React.Component{
 
@@ -16,9 +18,12 @@ export default class RegisterScreen extends React.Component{
       name: "",
       email: "",
       password: "",
-      avatar: null
+      //Fix it!
+      //Works on Android for some reason? But fix the emulator error
+      //avatar: null
     },
-    errorMessage: null
+    errorMessage: null,
+    switchValue: false
   };
 
   handlePickAvatar = async () => {
@@ -28,7 +33,7 @@ export default class RegisterScreen extends React.Component{
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3]
-    });
+    })
 
     if(!result.cancelled){
       this.setState({user: { ...this.state.user, avatar: result.uri} });
@@ -47,8 +52,19 @@ export default class RegisterScreen extends React.Component{
     .catch(error => this.setState({errorMessage: error.message}));
   };
 
+  toggleTerms = (value) => {
+    this.setState({switchValue : value})
+  }
+
 
   render() {
+    // let data = [{
+    //   value: 'Male',
+    // }, {
+    //   value: 'Female'
+    // }, {
+    //   value: 'Other'
+    // }];
     return (
       <View style={styles.container}>
         
@@ -71,7 +87,7 @@ export default class RegisterScreen extends React.Component{
           <Text style={styles.greeting}>{'Hello!\nSign up to get started.'}</Text>
 
           <TouchableOpacity style={styles.avatarPlaceholder} onPress={this.handlePickAvatar}>
-            <Image source={{uri: this.state.user.avatar}} style={styles.avatar}/>
+            <Image source={{uri:this.state.user.avatar}} style={styles.avatar}/>
             <Ionicons 
             name="ios-add" 
             size={40} 
@@ -84,7 +100,9 @@ export default class RegisterScreen extends React.Component{
         <View style={styles.errorMessage}>
           {this.state.errorMessage && <Text style={styles.error}>{this.state.errorMessage}</Text>}
         </View>
-
+        
+        {/* Make some fields mandatory */}
+        
         <View style={styles.form}>
           <View>
             <Text style={styles.inputTitle}>Full Name</Text>
@@ -118,6 +136,25 @@ export default class RegisterScreen extends React.Component{
               ></TextInput>
           </View>
 
+          {/* <View style={{marginTop: 32}}>
+            <Text style={styles.inputTitle}>Gender</Text>
+            <Dropdown
+            label='Select your gender'
+            data={data}
+            />
+          </View> */}
+
+          <View style={{marginTop: 32}}>
+            <Text style={styles.inputTitle}>I agree to the terms & conditions</Text>
+            <Switch
+            style={{marginTop: 5}}
+            trackColor={{false:"#767577", true:"#00d589"}}
+            thumbColor={{false:"#f5dd4b", true:"#00d589"}}
+            onValueChange = {this.toggleTerms}
+            value = {this.state.switchValue}
+            ></Switch>
+          </View>
+
         </View>
 
         <TouchableOpacity style={styles.button} onPress={this.handleSignUp}>
@@ -138,7 +175,7 @@ export default class RegisterScreen extends React.Component{
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1,
+    flex: 1
   },
   greeting:{
     marginTop: 5,
@@ -187,7 +224,7 @@ const styles = StyleSheet.create({
     height:100,
     backgroundColor: "#E1E2E6",
     borderRadius: 50,
-    marginTop: 48,
+    marginTop: 10,
     justifyContent: "center",
     alignItems: "center"
   },
